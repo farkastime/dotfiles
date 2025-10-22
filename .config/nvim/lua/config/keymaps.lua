dd4d4801a7d82d4321915438a4930a2c552f3d7e
farkastime <timothy.farkas@proton.me>
2025-10-21 22:20:56 -0600

    add btop, nvim configuration, global git config

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -3,3 +3,9 @@
 -- Add any additional keymaps here
 
 vim.keymap.set("i", "jk", "<ESC>l", { silent = true })
+-- vim.api.nvim_set_keymap("n", "<leader>t", ":ToggleTerm<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>tt", ":ToggleTerm<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>ts", ":TermSelect<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>tn", ":ToggleTermSetName<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>th", "100:TermExec cmd=htop name=htop<CR>", { noremap = true, silent = true })
+vim.keymap.set("t", "<esc>", [[<C-\><C-n>]])
